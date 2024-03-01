c8f17a0511d0c6c482189e55e46d1732c4077e36
Adam Lopez <adamf04@gmail.com>
2024-03-01 14:57:09 -0500

    Adding category and subcategory logic to the backend to retrieve categories and any subcategories based on matching category names

--- admin/ecomm-admin/app/Http/Controllers/Admin/CategoryController.php
+++ admin/ecomm-admin/app/Http/Controllers/Admin/CategoryController.php
@@ -4,13 +4,29 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\Subcategory;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function AllCategory() {
         $all_categories = Category::all();
+        $categoryDetailsArray = [];
 
-        return $all_categories;
+        foreach($all_categories as $category) {
+            $subcategory = Subcategory::where('category_name', $category['category_name'])->get();
+            
+            $category_item = [
+                'category_name'    => $category['category_name'],
+                'category_image'   => $category['category_image'],
+                'subcategory_name' => $subcategory,
+            ];
+
+            array_push( $categoryDetailsArray ,$category_item);
+        }
+
+
+
+        return $categoryDetailsArray;
     }
 }
