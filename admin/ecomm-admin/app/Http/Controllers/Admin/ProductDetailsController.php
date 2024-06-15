2d789eb18a4e3295f2854a5a36e353a853dfb866
Adam Lopez <adamf04@gmail.com>
2024-06-15 02:20:49 -0400

    Creating a request from product details that contains an id and queries the db to ensure there is a product being returned

--- admin/ecomm-admin/app/Http/Controllers/Admin/ProductDetailsController.php
+++ admin/ecomm-admin/app/Http/Controllers/Admin/ProductDetailsController.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\ProductList;
+use App\Models\ProductDetails;
 use Illuminate\Http\Request;
 
 class ProductDetailsController extends Controller
 {
-    
+    public function ProductDetails(Request $request) {
+        
+        $id = $request->id;
+        
+        // Get the product data from the details and list based on the
+        // product id from the request
+        $productDetails = ProductDetails::where('product_id', $id)->get();
+        $productList    = ProductList::where('id', $id)->get();
+
+        $item = [
+            'productDetails' => $productDetails,
+            'productList'    => $productList,
+        ];
+
+        return $item;
+    }    
 }
