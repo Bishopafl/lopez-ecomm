8301d55974ac537c1c77dd49de83301a3d9d0111
Adam Lopez <adamf04@gmail.com>
2024-06-15 02:20:07 -0400

    Creating Route for product details, adjusting comments

--- admin/ecomm-admin/routes/api.php
+++ admin/ecomm-admin/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ContactController;
+use App\Http\Controllers\Admin\ProductDetailsController;
 use App\Http\Controllers\Admin\ProductListController;
 use App\Http\Controllers\Admin\SiteInfoController;
 use App\Http\Controllers\Admin\SliderController;
@@ -24,18 +25,27 @@ use App\Http\Controllers\Admin\VisitorController;
 //     return $request->user();
 // });
 
-Route::get('/getVisitor', [VisitorController::class, 'GetVisitorDetails']); // Get Visitor Details Route
+// Get Visitor Details Route
+Route::get('/getVisitor', [VisitorController::class, 'GetVisitorDetails']); 
 
-Route::post('/contactDetails', [ContactController::class, 'ContactPageDetails']); // Contact Page Route
+// Contact Page Route
+Route::post('/contactDetails', [ContactController::class, 'ContactPageDetails']); 
 
-Route::get('/allsiteinfo', [SiteInfoController::class, 'AllSiteInfo']); // Site Info Route
+// Site Info Route
+Route::get('/allsiteinfo', [SiteInfoController::class, 'AllSiteInfo']); 
 
-Route::get('/allcategorydata', [CategoryController::class, 'AllCategory']); // Category Route
+// Category Route
+Route::get('/allcategorydata', [CategoryController::class, 'AllCategory']); 
 
+// Product List Routes
 Route::get('/productlistbyremark/{remark}', [ProductListController::class, 'ProductListByRemark']); // Product List By Remark Route
 
 Route::get('/productlistbycategory/{category}', [ProductListController::class, 'ProductListByCategory']); // Product List by Collection Route
 
 Route::get('/productlistbysubcategory/{category}/{subcategory}', [ProductListController::class, 'ProductListBySubcategory']); // Product List by Subcategory Route
 
-Route::get('/allslider', [SliderController::class, 'AllHomeSliders']); // Slider Route
+// Slider Routes
+Route::get('/allhomesliders', [SliderController::class, 'AllHomeSliders']);
+
+// Product Details Routes
+Route::get('/product-details/{id}', [ProductDetailsController::class, 'ProductDetails']);
